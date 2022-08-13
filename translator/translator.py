8e5375b5f65f87694b855598d31681cfea873efb
Steven Hulbert <sdh2110@g.rit.edu>
2022-08-13 13:31:18 -0400

    Adds formatting of mint transactions

--- translator/translator.py
+++ translator/translator.py
@@ -4,14 +4,21 @@ MINT_DATE = 'Date'
 MINT_DESC = 'Description'
 MINT_ORG_DESC = 'Original Description'
 MINT_AMOUNT = 'Amount'
-MINT_TRANS_TYPE = 'Transaction Type'
+MINT_TRANSACTION_TYPE = 'Transaction Type'
 MINT_CATEGORY = 'Category'
 MINT_ACCOUNT = 'Account Name'
 MINT_LABELS = 'Labels'
 MINT_NOTES = 'Notes'
 
-EXPECTED_MINT_HEADERS = [MINT_DATE, MINT_DESC, MINT_ORG_DESC, MINT_AMOUNT, MINT_TRANS_TYPE, MINT_CATEGORY, MINT_ACCOUNT,
+EXPECTED_MINT_HEADERS = [MINT_DATE, MINT_DESC, MINT_ORG_DESC, MINT_AMOUNT, MINT_TRANSACTION_TYPE, MINT_CATEGORY,
+                         MINT_ACCOUNT,
                          MINT_LABELS, MINT_NOTES]
+
+AMOUNT = 'Amount'
+DATE = 'Date'
+FOR_OR_FROM = 'For/From'
+METHOD = 'Monetary Method'
+OTHER_INFO = 'Other Info'
 
 
 def load_resources():
@@ -25,11 +32,36 @@ def check_headers(csv_reader):
         raise Exception(error_message)
 
 
+def translate_from_mint(mint_transaction):
+    transaction = dict()
+
+    if mint_transaction[MINT_TRANSACTION_TYPE] == 'debit':
+        transaction[AMOUNT] = '-' + mint_transaction[MINT_AMOUNT]
+    else:
+        transaction[AMOUNT] = mint_transaction[MINT_AMOUNT]
+
+    transaction[DATE] = mint_transaction[MINT_DATE]
+    transaction[FOR_OR_FROM] = mint_transaction[MINT_CATEGORY]
+    transaction[METHOD] = mint_transaction[MINT_ACCOUNT]
+    transaction[OTHER_INFO] = mint_transaction[MINT_NOTES]
+
+    return transaction
+
+
+def apply_mapping(mapping_function, list_of_data):
+    return list(map(mapping_function, list_of_data))
+
+
 def main():
-    with open('../Inbox/march_transactions.csv', newline='') as csv_file:
+    with open('../Inbox/transactions.csv', newline='') as transactions_file:
         load_resources()
-        csv_reader = csv.DictReader(csv_file, delimiter=',')
-        check_headers(csv_reader)
+        transactions_reader = csv.DictReader(transactions_file, delimiter=',')
+        check_headers(transactions_reader)
+
+        raw_transactions = list(transactions_reader)
+        formatted_transactions = apply_mapping(translate_from_mint, raw_transactions)
+
+        print(formatted_transactions)
 
 
 if __name__ == "__main__":
