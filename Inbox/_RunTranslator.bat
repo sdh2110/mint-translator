5056ff19b8787284c8429d12520a47acd5268286
Steven Hulbert <sdh2110@g.rit.edu>
2022-08-14 17:37:57 -0400

    Fixes the pause feature in the run scripts

--- Inbox/_RunTranslator.bat
+++ Inbox/_RunTranslator.bat
@@ -1,11 +1,13 @@
+@ECHO OFF
 MODE 150, 30
 
 python ../translator/translator.py
+set save_error=%ERRORLEVEL%
 
-IF exist "formatted_transactions.csv (
+IF exist "formatted_transactions.csv" (
     CLIP < formatted_transactions.csv
 )
 
-IF %ERRORLEVEL% NEQ 0 (
+IF %save_error% NEQ 0 (
     PAUSE
 )
